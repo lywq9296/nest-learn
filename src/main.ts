1882fc5eb00a1c817f2432234d0ec135f283e4fe
lywq9296 <ywli0314@sina.com>
2024-12-18 21:02:35 +0800

    feat: config npm 包实现配置的方案

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as config from 'config';
+const dbConfig = config.get('db');
+console.log(dbConfig, process.env);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
