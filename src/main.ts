a28dfb5e3cf72595c32728d4dfa1e782ab705257
lywq9296 <ywli0314@sina.com>
2024-12-18 20:45:25 +0800

    feat: dotenv  npm 包实现配置的方案

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(process.env);
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
